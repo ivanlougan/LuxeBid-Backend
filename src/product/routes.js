f8537378ecb33bfd2b9d0ab65dd20b2a49547741
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-21 16:25:16 +0000

    added routes for games and changed functions

--- src/product/routes.js
+++ src/product/routes.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 const productRouter = Router();
 
 const { hashPass, tokenCheck } = require("../middleware");
-const { getAllGames, updategame, deleteGame } = require("./controllers")
+const { getAllGames, updateGame, deleteGame } = require("./controllers")
 
 
 productRouter.get("/product/getGames", tokenCheck, getAllGames);
+productRouter.delete("./product/deletegame", tokenCheck, deleteGame);
+productRouter.put("./product/updategame", tokenCheck, updateGame);
 
 
 
