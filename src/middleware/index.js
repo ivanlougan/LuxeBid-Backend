9ad531476ff3e47c3971d5ae7ec17b3349ddad95
Robert Zdeb <robinio123@gmail.com>
2023-03-21 10:13:35 +0000

    hashing and decrypting password

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -12,4 +12,33 @@ const hashPass = async (req, res, next) => {
     }
 }
 
-const comparePass = async ()
+const comparePass = async (req, res, next) => {
+    try {
+        req.user = await User.findOne({ where: {username: req.body.username }});
+        const match = await bcrypt.compare( req.body.password, req.user.password );
+        if(!match) {
+            const error = new Error("Password do not match");
+            res.status(500).json({ errorMessage: error.message, error: error })
+        }
+        next();        
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+const tokenCheck = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization");
+        const decodedToken = await jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.findOne({ where: { id: decodedToken.id }});
+        if (!user) {
+             const error = new Error("User Not Authorized");
+             res.status(401).json({ eroorMessage: error.message, error: error})           
+        }
+        req.authCheck = user;
+        next();
+    } catch (error) {
+        res.status(501).json({ errorMessage: "failure", error: error});
+    }
+}
+module.exports = { hashPass, tokenCheck, comparePass }
--- src/users/routes.js
+++ src/users/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const userRouter = Router();
 
+const { comparePass, tokenCheck, hashPass} = require("../middleware");
 const { registerUser, login } = require("./controllers");
 
-userRouter.post( "/users/registeruser", registerUser );
-userRouter.post( "/users/login", login );
+userRouter.post( "/users/registeruser", hashPass, registerUser );
+userRouter.post( "/users/login", comparePass, login );
 
 module.exports = userRouter;
