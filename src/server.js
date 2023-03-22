9384f8017fa5c284deb2fe247529255e830d533e
K Burnett <bluemalek@gmail.com>
2023-03-22 09:45:51 +0000

    FIXED: api headers hidden

--- src/server.js
+++ src/server.js
@@ -32,8 +32,8 @@ app.post("/getGames", (req, res) => {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'Client-ID': 'zzsc5u3kro28y6yizq1zjq27vxwce7',
-            "Authorization": "Bearer 1zngaaqeohmi5ikt5vui3hfq971h2g"
+            'Client-ID': `${process.env.Client_ID}`,
+            "Authorization": `Bearer ${process.env.API_token}`
         },
         data:
             "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
