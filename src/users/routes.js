5dd6c0568749e68b4d968b83a44391d0e2fe72ee
Robert Zdeb <robinio123@gmail.com>
2023-03-29 11:48:34 +0100

    auth route added

--- src/users/routes.js
+++ src/users/routes.js
@@ -4,6 +4,8 @@ const userRouter = Router();
 const { comparePass, tokenCheck, hashPass} = require("../middleware");
 const { registerUser, login, updateUser, deleteUser } = require("./controllers");
 
+userRouter.get( "/users/authcheck", tokenCheck, login);
+
 userRouter.post( "/users/registeruser", hashPass, registerUser );
 userRouter.post( "/users/login", comparePass, login );
 
